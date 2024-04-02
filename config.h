9cefd7442e5ef5394a00288523d5d9812cc0ea24
Riccardo Finotello <riccardo.finotello@gmail.com>
2024-04-02 22:02:15 +0200

    Bigger fonts

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "DejaVuSansM Nerd Font:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = { "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
